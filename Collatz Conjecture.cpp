f11a56172ece31fab5beef538dc63e849e05bc1c
William <williamjensenbc@gmail.com>
2022-02-03 22:37:47 +0100

    Initial commit Initial commit using 3D example 8 (https://github.com/BetaChess/VulkanLearning) as the base.

---
+++ Collatz Conjecture.cpp
@@ -0,0 +1,12 @@
+﻿// Collatz Conjecture.cpp : Defines the entry point for the application.
+//
+
+#include "Collatz Conjecture.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
